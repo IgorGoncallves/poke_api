b791f0bf9d39b90b6c79de528a76f0b582c289b7
IgorGoncallves <emp.igorpereira@gmail.com>
2025-10-14 19:33:05 -0300

    chore(config): atualizar conexões e variáveis de ambiente do projeto

--- configuracoes/variaveis_env.py
+++ configuracoes/variaveis_env.py
@@ -9,3 +9,10 @@ load_dotenv(dotenv_path=env)
 api_url = os.getenv("api_url")
 api_usuario = os.getenv("api_usuario") 
 api_senha = os.getenv("api_senha")
+
+
+DB_HOST = os.getenv("DB_HOST")
+DB_PORT = os.getenv("DB_PORT")
+DB_NAME = os.getenv("DB_NAME")
+DB_USER = os.getenv("DB_USER")
+DB_PASSWORD = os.getenv("DB_PASSWORD")

75629c4d7eac9c42c3978745213860acd71a61f4
IgorGoncallves <emp.igorpereira@gmail.com>
2025-10-15 10:30:42 -0300

    feat(etl): criar função para carga de dados JSON na camada stage

---
+++ etl/carga/carga_json_stage.py
@@ -0,0 +1,94 @@
+import json
+from pathlib import Path
+from configuracoes.conexao_db import get_connection
+import time
+
+def carregar_pokemons():
+    caminho = Path("data/raw/pokemon_attributes.json")
+    if not caminho.exists():
+        print(" Arquivo pokemons.json não encontrado")
+        return
+    
+    with open(caminho, "r", encoding="utf-8") as f:
+        pokemons = json.load(f)
+
+    conn = get_connection()
+    cur = conn.cursor()
+
+    for p in pokemons:
+        cur.execute("""
+            INSERT INTO stage.stg_pokemon (
+                id, name, hp, attack, defense,
+                sp_attack, sp_defense, speed,
+                generation, legendary, types
+            )
+            VALUES (%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s)
+            ON CONFLICT (id) DO NOTHING;
+        """, (
+            p.get("id"),
+            p.get("name"),
+            p.get("hp"),
+            p.get("attack"),
+            p.get("defense"),
+            p.get("sp_attack"),
+            p.get("sp_defense"),
+            p.get("speed"),
+            p.get("generation"),
+            p.get("legendary"),
+            p.get("types")
+        ))
+    
+    conn.commit()
+    cur.close()
+    conn.close()
+    print(" Dados de Pokémon carregados com sucesso para stage.stg_pokemon")
+
+
+def carregar_batalhas():
+    caminho = Path("data/raw/battles_history.json")
+    if not caminho.exists():
+        print(" Arquivo batalhas.json não encontrado")
+        return
+    
+    with open(caminho, "r", encoding="utf-8") as f:
+        batalhas = json.load(f)
+
+    conn = get_connection()
+    cur = conn.cursor()
+
+    for b in batalhas:
+        cur.execute("""
+            INSERT INTO stage.stg_batalha (
+                first_pokemon, second_pokemon, winner
+            )
+            VALUES (%s,%s,%s);
+        """, (
+            b.get("first_pokemon"),
+            b.get("second_pokemon"),
+            b.get("winner")
+        ))
+    
+    conn.commit()
+    cur.close()
+    conn.close()
+    print(" Dados de batalhas carregados com sucesso para stage.stg_batalha")
+
+
+if __name__ == "__main__":
+    
+    print(" Iniciando carga para camada STAGE")
+    start1 = time.time()
+    carregar_pokemons()
+    end1 = time.time()
+
+    print(f"\n Carga pokemons em: {end1 - start1:.2f} segundos")
+
+    start2 = time.time()
+    carregar_batalhas()
+    end2 = time.time()
+
+    print(f"\n Carga batalhas em: {end2 - start2:.2f} segundos")
+
+    print(f"\n Processo total finalizado em: {end2 - start1:.2f} segundos")
+
+
